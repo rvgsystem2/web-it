4c0c91312f732f17b59b334c43701789b81f6e85
rvgsystem2 <rvgsystem2@gmail.com>
2025-06-02 16:55:24 +0530

    add links at footer

--- resources/views/component/footer.blade.php
+++ resources/views/component/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="row gx-5">
             <div class="col-lg-4 col-md-6 footer-about">
                 <div class="d-flex flex-column align-items-center justify-content-center text-center h-100 bg-primary p-4">
-                    <a href="index.html" class="navbar-brand">
+                    <a href="/" class="navbar-brand">
                         <h1 class="m-0 text-white"><i class="me-2"></i>Cybrexus</h1>
                     </a>
                     <p class="mt-3 mb-4">We build a better future with the tech</p>
@@ -44,9 +44,9 @@
                         </div>
                         <div class="link-animated d-flex flex-column justify-content-start">
                             <a class="text-light mb-2" href="/"><i class="bi bi-arrow-right text-primary me-2"></i>Home</a>
-                            <a class="text-light mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Our Services</a>
-                            <a class="text-light mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Meet The Team</a>
-                            <a class="text-light mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Latest Blog</a>
+                            <a class="text-light mb-2" href="{{route('service')}}"><i class="bi bi-arrow-right text-primary me-2"></i>Our Services</a>
+                            <a class="text-light mb-2" href="{{route('team')}}"><i class="bi bi-arrow-right text-primary me-2"></i>Meet The Team</a>
+                            <a class="text-light mb-2" href="{{route('blog')}}"><i class="bi bi-arrow-right text-primary me-2"></i>Latest Blog</a>
                         </div>
                     </div>
                     <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
@@ -54,11 +54,11 @@
                             <h3 class="text-light mb-0">Information</h3>
                         </div>
                         <div class="link-animated d-flex flex-column justify-content-start">
-                            <a class="text-light mb-2" href="about.php"><i class="bi bi-arrow-right text-primary me-2"></i>About Us</a>
-                            <a class="text-light mb-2" href="contact.php"><i class="bi bi-arrow-right text-primary me-2"></i>Contact Us</a>
-                            <a class="text-light mb-2" href="#"><i class="bi bi-arrow-right text-primary me-2"></i>Blog</a>
-                            <a class="text-light mb-2" href="Privacy_Policy.php"><i class="bi bi-arrow-right text-primary me-2"></i>Privacy Policy</a>
-                            <a class="text-light mb-2" href="Term&Conditions.php"><i class="bi bi-arrow-right text-primary me-2"></i>Terms & Conditions</a>
+                            <a class="text-light mb-2" href="{{route('about')}}"><i class="bi bi-arrow-right text-primary me-2"></i>About Us</a>
+                            <a class="text-light mb-2" href="{{route('contact')}}"><i class="bi bi-arrow-right text-primary me-2"></i>Contact Us</a>
+                            <a class="text-light mb-2" href="{{route('blog')}}"><i class="bi bi-arrow-right text-primary me-2"></i>Blog</a>
+                            <a class="text-light mb-2" href="{{route('privacy_policy')}}"><i class="bi bi-arrow-right text-primary me-2"></i>Privacy Policy</a>
+                            <a class="text-light mb-2" href="{{route('terms_condition')}}"><i class="bi bi-arrow-right text-primary me-2"></i>Terms & Conditions</a>
                         </div>
                     </div>
                 </div>
@@ -71,7 +71,7 @@
         <div class="row justify-content-end">
             <div class="col-lg-8 col-md-6">
                 <div class="d-flex align-items-center justify-content-center" style="height: 75px;">
-                    <p class="mb-0">&copy; <a class="text-white border-bottom" href="#">CYBREXUS</a>. All Rights Reserved. Designed by <a class="text-white border-bottom" href="https://www.cybrexus.com/">CYBREXUS</a></p>
+                    <p class="mb-0">&copy; <a class="text-white border-bottom" href="/">CYBREXUS</a>. All Rights Reserved. Designed by <a class="text-white border-bottom" href="https://www.cybrexus.com/">CYBREXUS</a></p>
                 </div>
             </div>
         </div>
@@ -80,7 +80,7 @@
 <!-- Footer End -->
 
 <!-- Back to Top -->
-<a href="#" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
+<a href="/" class="btn btn-lg btn-primary btn-lg-square rounded back-to-top"><i class="bi bi-arrow-up"></i></a>
 
 <!-- JavaScript Libraries -->
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
