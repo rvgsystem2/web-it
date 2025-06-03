57e547de811bfb9b075503f3a7859ef22d0bc989
chandan <chandandigital6@gmail.com>
2025-06-03 16:52:30 +0530

    about

---
+++ database/migrations/2025_06_03_095309_create_abouts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('f_1')->nullable();
+            $table->string('f_2')->nullable();
+            $table->string('f_3')->nullable();
+            $table->string('f_4')->nullable();
+            $table->string('call_to_title')->nullable();
+            $table->string('call_to_number')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('abouts');
+    }
+};
